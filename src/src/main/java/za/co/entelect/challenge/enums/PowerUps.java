8294b2500528e4d10895b3f160a24184a8ac7875
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-02-05 09:56:13 +0700

    lengkapin enums State, PowerUps, Terrain

---
+++ src/src/main/java/za/co/entelect/challenge/enums/PowerUps.java
@@ -0,0 +1,16 @@
+package za.co.entelect.challenge.enums;
+
+import com.google.gson.annotations.SerializedName;
+
+public enum PowerUps {
+    @SerializedName("BOOST")
+    BOOST,
+    @SerializedName("OIL")
+    OIL,
+    @SerializedName("LIZARD")
+    LIZARD,
+    @SerializedName("TWEET")
+    TWEET,
+    @SerializedName("EMP")
+    EMP
+}
