24cdd7ad141f5520d845d5f8d00e9362a6646082
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-02-18 23:04:06 +0700

    apus yang gaperlu

--- src/src/main/java/za/co/entelect/challenge/Main.java
+++ src/src/main/java/za/co/entelect/challenge/Main.java
@@ -1,10 +1,8 @@
 package za.co.entelect.challenge;
 
 import com.google.gson.Gson;
-import za.co.entelect.challenge.command.Command;
 import za.co.entelect.challenge.entities.GameState;
-import za.co.entelect.challenge.entities.Lane;
-import za.co.entelect.challenge.enums.PowerUps;
+
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -28,45 +26,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Gson gson = new Gson();
         Random random = new Random(System.nanoTime());
-//        int roundNumber = 0;
 
         while(true) {
             try {
                 int roundNumber = sc.nextInt();
-//                ++roundNumber;
                 String statePath = String.format("./%s/%d/%s", ROUNDS_DIRECTORY, roundNumber, STATE_FILE_NAME);
-//                String statePath = String.format("target/%s/%d/%s", ROUNDS_DIRECTORY, roundNumber, STATE_FILE_NAME);
                 String state = new String(Files.readAllBytes(Paths.get(statePath)));
 
                 GameState gameState = gson.fromJson(state, GameState.class);
-//                printPowerUps(gameState);
-//                printWorldMap(gameState);
                 String command = new Bot(random, gameState).run();
                 System.out.println(String.format("C;%d;%s", roundNumber, command));
-//                prevSpeed = gameState.player.speed;
-//                break;
-//                if(roundNumber==133) break;
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    public static void printWorldMap(GameState gS) {
-        int i=0;
-        for(Lane[] lanei : gS.lanes) {
-            System.out.println("Lane[] ke-"+i);
-            i +=1;
-            System.out.println("banyak petak: "+lanei.length);
-            for(Lane petak : lanei) {
-                System.out.println(petak.terrain);
-            }
-        }
-    }
-
-    public static void printPowerUps(GameState gS) {
-        for(PowerUps p : gS.player.powerups) {
-            System.out.println(p);
         }
     }
 }
